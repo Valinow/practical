183a02ab7c1bdb9231f5ef702184b251ec4d8b6e
Valinow <M.Vasko_FIT_4_22_B_d@knute.edu.ua>
2023-05-08 22:05:12 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        Console.WriteLine("Введите число: ");
+
+        string numberString = Console.ReadLine();
+
+        int maxDigit = int.MinValue;
+
+        int minDigit = int.MaxValue;
+
+        for (int i = 0; i < numberString.Length; i++)
+
+        {
+
+            int digit = int.Parse(numberString[i].ToString());
+
+            if (digit > maxDigit)
+
+            {
+
+                maxDigit = digit;
+
+            }
+
+            if (digit < minDigit)
+
+            {
+
+                minDigit = digit;
+
+            }
+
+        }
+
+        Console.WriteLine("Максимальная цифра: " + maxDigit);
+
+        Console.WriteLine("Минимальная цифра: " + minDigit);
+
+        Console.ReadKey();
+
+    }
+
+}
